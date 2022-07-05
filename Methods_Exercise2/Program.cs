46bd147878a2c9808bfbbab0aaade77a90f43949
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-05 11:20:48 -0500

    Initial commit on Methods_Excercise 2

---
+++ Methods_Exercise2/Program.cs
@@ -0,0 +1,5 @@
+﻿using Methods_Exercise2;
+
+Console.WriteLine(Simple_Calculator.Sum(2, 4));
+Console.WriteLine(Simple_Calculator.Sum(2, 4, 6));
+Console.WriteLine(Simple_Calculator.Sum(1, 1, 1, 1, 1));
---
+++ Methods_Exercise2/Simple Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise2
+{
+    internal class Simple_Calculator
+    { public static int Sum(int x, int y)
+        { return x + y; }
+        public static int Sum(int x, int y, int z)
+        {
+
+            return x + y + z;
+        }
+        public static int Sum(int a, int b, int c, int d, int e)
+        { return a + b + c + d + e; }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Divide(int x, int y)
+        { return x / y; }
+
+
+    }
+}
